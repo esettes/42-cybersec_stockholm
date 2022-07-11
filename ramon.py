1535ed36d884a71910f00747c7dacc23f059e501
esettes <ioanaroxana.st@gmail.com>
2022-07-11 20:13:08 +0200

    add functs to generate crypto key

--- ramon.py
+++ ramon.py
@@ -1,5 +1,4 @@
 import os
-from cryptography.fernet import Fernet
 
 def main():
 
@@ -11,16 +10,6 @@ def main():
         print(filename, src)
         if not filename.endswith('.ft'):
             os.rename(src, dst)
-
-def write_key():
-
-    key = Fernet.generate_key()
-    with open("key.key", "wb") as key_file:
-        key_file.write(key)
-
-def load_key():
-
-    return open("key.key", "rb").read()
 
 if __name__ == '__main__':
     main()
